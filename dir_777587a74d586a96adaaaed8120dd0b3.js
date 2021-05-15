5809f432fbd9a881351eed33428b750333afe8cc
Allan Bowe <allnbwe@gmail.com>
2021-05-15 14:43:24 +0300

    build.sh build on 2021-05-15:14:43:24

--- dir_777587a74d586a96adaaaed8120dd0b3.js
+++ dir_777587a74d586a96adaaaed8120dd0b3.js
@@ -5,5 +5,7 @@ var dir_777587a74d586a96adaaaed8120dd0b3 =
     [ "mv_getjoblog.test.sas", "mv__getjoblog_8test_8sas.html", "mv__getjoblog_8test_8sas" ],
     [ "mv_getjobresult.test.sas", "mv__getjobresult_8test_8sas.html", "mv__getjobresult_8test_8sas" ],
     [ "mv_jobflow.test.1.sas", "mv__jobflow_8test_81_8sas.html", "mv__jobflow_8test_81_8sas" ],
-    [ "mv_jobflow.test.2.sas", "mv__jobflow_8test_82_8sas.html", "mv__jobflow_8test_82_8sas" ]
+    [ "mv_jobflow.test.2.sas", "mv__jobflow_8test_82_8sas.html", "mv__jobflow_8test_82_8sas" ],
+    [ "mv_registerclient.test.1.sas", "mv__registerclient_8test_81_8sas.html", "mv__registerclient_8test_81_8sas" ],
+    [ "mv_registerclient.test.2.sas", "mv__registerclient_8test_82_8sas.html", "mv__registerclient_8test_82_8sas" ]
 ];
