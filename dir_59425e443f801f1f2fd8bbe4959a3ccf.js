57a35fb7eac1bd07f6819b0137ea52ec50301535
Allan Bowe <allnbwe@gmail.com>
2021-08-18 19:48:32 +0300

    build.sh build on 2021-08-18:19:48:32

--- dir_59425e443f801f1f2fd8bbe4959a3ccf.js
+++ dir_59425e443f801f1f2fd8bbe4959a3ccf.js
@@ -1,5 +1,4 @@
 var dir_59425e443f801f1f2fd8bbe4959a3ccf =
 [
-    [ "crossplatform", "dir_20d75ed6d8a9fdfee91e4f9ba91fc8ef.html", "dir_20d75ed6d8a9fdfee91e4f9ba91fc8ef" ],
-    [ "viyaonly", "dir_650092e11941d85fd99c2002d68de369.html", "dir_650092e11941d85fd99c2002d68de369" ]
+    [ "crossplatform", "dir_20d75ed6d8a9fdfee91e4f9ba91fc8ef.html", "dir_20d75ed6d8a9fdfee91e4f9ba91fc8ef" ]
 ];
--- mv__getjoblog_8test_8sas.js
+++
@@ -1,4 +0,0 @@
-var mv__getjoblog_8test_8sas =
-[
-    [ "temp", "mv__getjoblog_8test_8sas.html#a392988bf52a872a94b042e56b7ac15cb", null ]
-];
