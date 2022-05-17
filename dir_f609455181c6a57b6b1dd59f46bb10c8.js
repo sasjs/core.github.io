fa23aea14a4a5f533a59fe06ead9cbecc1fe8485
munja <allnbwe@gmail.com>
2022-05-17 16:12:40 +0100

    build.sh build on 2022-05-17:16:12:40

--- dir_f609455181c6a57b6b1dd59f46bb10c8.js
+++ dir_f609455181c6a57b6b1dd59f46bb10c8.js
@@ -2,10 +2,12 @@ var dir_f609455181c6a57b6b1dd59f46bb10c8 =
 [
     [ "mfs_httpheader.test.sas", "mfs__httpheader_8test_8sas.html", null ],
     [ "ms_createfile.test.sas", "ms__createfile_8test_8sas.html", null ],
+    [ "ms_creategroup.test.sas", "ms__creategroup_8test_8sas.html", null ],
     [ "ms_createuser.test.sas", "ms__createuser_8test_8sas.html", null ],
     [ "ms_createwebservice.test.sas", "ms__createwebservice_8test_8sas.html", null ],
     [ "ms_deletefile.test.sas", "ms__deletefile_8test_8sas.html", null ],
     [ "ms_getfile.test.sas", "ms__getfile_8test_8sas.html", null ],
+    [ "ms_getgroups.test.sas", "ms__getgroups_8test_8sas.html", null ],
     [ "ms_getusers.test.sas", "ms__getusers_8test_8sas.html", null ],
     [ "ms_runstp.test.sas", "ms__runstp_8test_8sas.html", null ],
     [ "ms_testservice.test.sas", "ms__testservice_8test_8sas.html", null ],
