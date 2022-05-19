2a0ac8a9e6468ca7fe983b51886cd7170c807bfb
munja <allnbwe@gmail.com>
2022-05-19 21:25:55 +0100

    build.sh build on 2022-05-19:21:25:55

--- navtreedata.js
+++ navtreedata.js
@@ -33,8 +33,8 @@ var NAVTREE =
 var NAVTREEINDEX =
 [
 "dir_0d92069d5a23b1d3125c326d74f1a07c.html",
-"mm__getfoldertree_8sas_source.html",
-"mp__makedata_8test_8sas_source.html"
+"mm__webout_8test_8sas.html",
+"mv__getjobresult_8sas.html"
 ];
 
 var SYNCONMSG = 'click to disable panel synchronisation';
