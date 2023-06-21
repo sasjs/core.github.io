6642cd8ccd6cef5aeb03c6fa614ba653e2d3ae2e
Allan <allan@4gl.io>
2023-06-21 16:49:09 +0100

    build.sh build on 2023-06-21:16:49:09

---
+++ mp__loadformat_8test_81_8sas.js
@@ -0,0 +1,5 @@
+var mp__loadformat_8test_81_8sas =
+[
+    [ "mp_assert", "mp__loadformat_8test_81_8sas.html#ab128f5eb4d7bf01ab3a930fbb9898632", null ],
+    [ "mp_assertscope", "mp__loadformat_8test_81_8sas.html#a03e28494cd3c499de1c71864655d9687", null ]
+];
--- navtreedata.js
+++ navtreedata.js
@@ -35,7 +35,7 @@ var NAVTREEINDEX =
 "dir_0d92069d5a23b1d3125c326d74f1a07c.html",
 "mm__assigndirectlib_8sas_source.html",
 "mp__ds2csv_8sas.html",
-"mp__streamfile_8test_8sas_source.html"
+"mp__streamfile_8sas.html"
 ];
 
 var SYNCONMSG = 'click to disable panel synchronisation';
